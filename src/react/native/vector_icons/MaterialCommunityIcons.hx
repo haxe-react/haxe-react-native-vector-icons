c601cd8a5ca80f29c299f420c184d03b175242ef
Kevin Leung <kevinresol@gmail.com>
2019-03-29 15:23:32 +0800

    Add MaterialCommunityIcons

---
+++ src/react/native/vector_icons/MaterialCommunityIcons.hx
@@ -0,0 +1,11 @@
+package react.native.vector_icons;
+
+import react.ReactComponent;
+
+@:jsRequire('react-native-vector-icons/MaterialCommunityIcons', 'default')
+extern class MaterialCommunityIcons extends ReactComponentOfProps<IconProps> {
+	public static inline var ToolbarAndroid = MaterialCommunityIconsToolbarAndroid;
+}
+
+@:jsRequire('react-native-vector-icons/MaterialCommunityIcons', 'default.ToolbarAndroid')
+extern class MaterialCommunityIconsToolbarAndroid extends ReactComponentOfProps<{}> {}
